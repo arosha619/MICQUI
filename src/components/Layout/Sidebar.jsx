7e35129b072e95b6825d8a9245aa371002dfa180
arosha619 <90087992+arosha619@users.noreply.github.com>
2023-09-17 10:13:07 +0530

    logout alert added

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./Layout.css";
 import logo from "../../Assets/logo/logo.png";
 import logoOnly from "../../Assets/logo/logoOnly.png";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink,useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Modal, Button } from "react-bootstrap";
+import { FaExclamationCircle, FaCheckCircle } from "react-icons/fa";
 import {
   faBars,
   faBarsStaggered,
@@ -14,6 +16,8 @@ import {
 
 function Sidebar() {
   const [isshow, setIsshow] = useState(true);
+  const [logoutModal, setLogoutModal] = useState(false);
+  const navigate = useNavigate();
 
   const toggleSidebar = () => {
     setIsshow(!isshow);
@@ -112,7 +116,7 @@ function Sidebar() {
           />
           {isshow && "Bucket List"}
         </NavLink>
-        <Link to="/">
+        <Link onClick={()=>setLogoutModal(true)}>
           <div className="menu-button logOut">
             <FontAwesomeIcon
               icon={faRightFromBracket}
@@ -123,10 +127,40 @@ function Sidebar() {
                 paddingRight: "20px",
               }}
             />
-            {isshow && "Log Out"}
+            {isshow && "Log Out"} 
           </div>
         </Link>
       </div>
+      {logoutModal && (
+            <Modal
+              style={{ background: "rgba(15, 14, 14, 0.144)" }}
+              show={logoutModal}
+              onHide={() => setLogoutModal(false)}
+            >
+              <Modal.Header closeButton className="text-center">
+                <div className="d-flex justify-content-center align-items-center text-danger">
+                  <FaExclamationCircle
+                    size={24}
+                    style={{ marginLeft: "220px" }}
+                  />
+                </div>
+              </Modal.Header>
+              <Modal.Body className="d-flex justify-content-center ">
+                Are you sure you want to Logout ?
+              </Modal.Body>
+              <Modal.Footer className="d-flex justify-content-center">
+                <Button
+                  variant="secondary"
+                  onClick={() => setLogoutModal(false)}
+                >
+                  No
+                </Button>
+                <Button variant="dark"onClick={() => navigate("/")}>
+                  Yes
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          )}
     </div>
   );
 }
